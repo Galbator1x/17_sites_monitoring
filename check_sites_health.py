5e77fa85e44fd345737d04b6583a50d9e4d2bf3e
Ivan Filimonov <snake73w@gmail.com>
2016-10-06 04:45:48 +0700

    Added script.

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,73 @@
-def load_urls4check(path):
-    pass
+import requests
+import pythonwhois
+import os
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
+from sys import argv
+
+
+def get_filepath_from_argv(argv):
+    return argv[1]
+
+
+def load_urls_for_check(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath) as file_handler:
+        return file_handler.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    if requests.head(url).status_code == 200:
+        return 'Yes'
+    return 'No'
+
+
+def get_domain_from_url(url):
+    return urlparse(url).netloc
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    exp_date = pythonwhois.get_whois(domain_name).get('expiration_date', None)
+    return exp_date[0] if exp_date is not None else None
+
+
+def is_domain_paid_for_month_ahead(expiration_date):
+    if expiration_date is None:
+        return 'No data'
+    domain_is_paid = datetime.now() <= expiration_date - timedelta(days=30)
+    return 'Yes' if domain_is_paid else 'No'
+
+
+def check_sites_health(urls_list):
+    sites_health = []
+    for url in urls_list:
+        server_works = 'Yes' if is_server_respond_with_200(url) else 'No'
+        exp_date = get_domain_expiration_date(get_domain_from_url(url))
+        domain_paid = is_domain_paid_for_month_ahead(exp_date)
+        sites_health.append([server_works, domain_paid, url])
+    return sites_health
+
+
+def output_sites_health_to_console(sites_health):
+    for site in sites_health:
+        print('server works: {}, domain is paid for a month ahead: {}  {}'.
+              format(site[0], site[1], site[2]))
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = get_filepath_from_argv(argv)
+    except IndexError:
+        print('Enter the path to the list of urls by first parameter.')
+        exit()
+
+    urls = load_urls_for_check(filepath)
+    if urls is None:
+        print('File does not exists.')
+        exit()
+
+    try:
+        output_sites_health_to_console(check_sites_health(urls))
+    except (ConnectionResetError, ConnectionError):
+        print('Failed to connect, try later')
